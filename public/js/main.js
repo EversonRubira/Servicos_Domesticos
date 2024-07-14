1ab9a4e70ef7f677bcfbe9262be1a9ae54452a51
EversonRubira <eversonrubira@gmail.com>
2024-07-14 09:05:49 +0100

    update commit Servico_Domesticos

---
+++ public/js/main.js
@@ -0,0 +1,20 @@
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    if (evt) {
+        evt.currentTarget.className += " active";
+    }
+}
+
+// Configuração adequada para quando a página carrega
+document.addEventListener('DOMContentLoaded', function() {
+    openTab(null, 'Login');  // Abre a aba 'Login' por padrão quando a página carrega
+});
---
+++ src/app.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// Importa rotas
+const router = require('./routes/userRoutes');
+
+// Conexão com o banco de dados
+const db = require('./config/database');
+
+// Middleware para servir arquivos estáticos da pasta 'public'
+// Certifique-se de que o caminho esteja correto conforme a localização da pasta 'public' em relação a este arquivo
+app.use(express.static(path.join(__dirname, '../public')));
+
+// Middleware para parsear JSON e urlencoded data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Usando o router
+app.use(router);
+
+// Rota para servir a página inicial
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public', 'index.html'));
+});
+
+// Rota adicional para servir a página de login
+app.get('/login_register.html', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public', 'login_register.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
