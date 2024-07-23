4329e7533f9c5e49dc4f65563d49ef8635f7ad1c
carloserpa <carlo.serpa.it@gmail.com>
2024-07-23 22:30:16 +0200

    All testes checked

---
+++ __tests__/index.test.js
@@ -0,0 +1,11 @@
+describe('User Tests', () => {
+    require('./user.api.test');
+});
+  
+describe('Service Tests', () => {
+    require('./service.api.test');
+});
+
+describe('Appointment Tests', () => {
+    require('./appointment.api.test');
+});
